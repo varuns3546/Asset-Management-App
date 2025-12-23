3aa427f317ab7776a463d6c8266380c235dee443
BV TX Station 3 <station3@bv-tx.local>
2025-12-23 14:49:32 -0600

    d

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 import React from 'react'
-import OpenProjectModal from './OpenProjectModal'
+import MyProjectsModal from './MyProjectsModal'
 import CreateProjectModal from './CreateProjectModal'
 import '../styles/navbar.css'
 
@@ -19,10 +19,10 @@ const Dropdown = ({title, options, isOpen, onToggle, onOpenModal, onCloseModal, 
         }
         
         // Handle modal options only for Projects dropdown
-        if (onOpenModal && (option === 'Open Project' || option === 'Create Project')) {
+        if (onOpenModal && (option === 'My Projects' || option === 'Create Project')) {
             console.log('Opening modal for:', option) // Debug log
-            if (option === 'Open Project') {
-                onOpenModal(<OpenProjectModal onClose={onCloseModal} />, 'Open Project')
+            if (option === 'My Projects') {
+                onOpenModal(<MyProjectsModal onClose={onCloseModal} />, 'My Projects')
             } else if (option === 'Create Project') {
                 onOpenModal(<CreateProjectModal onClose={onCloseModal} />, 'Create Project')
             }
